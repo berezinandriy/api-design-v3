13a2fdcc04a1427a14035b4e377a622ea5260c5b
berezinandriy <berezinandriy@gmail.com>
2019-10-07 14:57:50 -0700

    create controllers

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -2,4 +2,19 @@ import { Router } from 'express'
 
 const router = Router()
 
+router
+  .route('/')
+  .get((req, res)=> res.send("get"))
+  .post((req, res)=> res.send("post"))
+  .put((req, res)=> res.send("put"))
+  .delete((req, res)=> res.send("delete"))
+
+router
+  .route('/:id')
+  .get((req, res)=> res.send(req.params.id))
+  .post((req, res)=> res.send("post item"))
+  .put((req, res)=> res.send("put item"))
+  .delete((req, res)=> res.send("delete item"))
+
 export default router
+
